b7533c9d2e6a4deb4883c5c26fa9be2932815fea
chenshiman <shiman.c@sumian.com>
2021-06-30 00:58:57 +0800

    完成content脚本

---
+++ src/content/index.js
@@ -0,0 +1,3 @@
+var meatballImg = chrome.extension.getURL("assets/images/meatball.png")
+var style = 'background-image: url(' + meatballImg + ')!important'
+document.body.setAttribute('style', style)
